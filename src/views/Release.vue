b8e2c2d25e0bc594595649fbc5229d7623810399
NataliaTepluhina <tarya.se@gmail.com>
2020-09-20 20:25:45 +0300

    feat: populated release view

--- src/views/Release.vue
+++ src/views/Release.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="release md:flex shadow-lg w-full">
-    RELEASE
     <img
-      class="h-full w-full md:w-1/3  object-cover rounded-lg rounded-r-none pb-5/6"
-      src=""
+      class="h-full max-h-full w-full md:w-1/3 rounded-lg rounded-r-none pb-5/6"
+      :src="release.picture"
       alt="Anime picture"
     />
     <div class="w-full md:w-2/3 px-4 py-4 bg-white rounded-lg">
       <div class="flex items-center">
         <h2 class="text-4xl text-gray-800 font-medium mr-auto">
-          <!-- name -->
+          {{ release.name }}
         </h2>
         <p class="text-gray-800 font-semibold tracking-tighter">
-          <!-- release date -->
+          {{ release.releaseDate }}
         </p>
       </div>
       <p class="text-xl text-gray-800 mt-4">
-        <!-- version -->
+        {{ release.version }}
       </p>
       <a
         class="text-2xl text-green-900 mt-6"
-        href="#"
+        :href="release.link"
         target="_blank"
         noopener
         noreferrer
@@ -32,11 +31,24 @@
 </template>
 
 <script>
-export default {}
+import releaseList from '../assets/db'
+export default {
+  releaseList,
+  computed: {
+    release() {
+      return this.$options.releaseList.find(r => r.id == this.$route.params.id)
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
 .release {
-  max-height: 300px;
+  max-height: 500px;
+}
+
+img {
+  max-height: 500px;
+  object-fit: cover;
 }
 </style>

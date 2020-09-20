9049037ed4064745df5c0afd9cafd9bae813667e
NataliaTepluhina <tarya.se@gmail.com>
2020-09-20 15:49:41 +0300

    fix: fixed app.vue structure

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,14 @@
         </li>
       </ul>
     </nav>
-    <section class="container flex flex-wrap mx-auto">
-      <ReleaseCard
-        v-for="release in $options.releasesList"
-        :key="release.version"
-        v-bind="release"
-      />
+    <section class="container mx-auto">
+      <div class="flex flex-wrap">
+        <ReleaseCard
+          v-for="release in $options.releasesList"
+          :key="release.version"
+          v-bind="release"
+        />
+      </div>
     </section>
   </main>
 </template>
